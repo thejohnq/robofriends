8228dce2aac7ff09cfe56b7ad97880b293389992
thejohnq <floschweig@gmail.com>
2022-07-08 08:54:43 +0200

    226. Building A React App 1

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Mr. Bot',
+        username: 'bot',
+        email: 'bot@robots.de'
+    },
+    {
+        id: 2,
+        name: 'Mrs. Droid',
+        username: 'droid',
+        email: 'droid@robots.de'
+    },
+    {
+        id: 3,
+        name: 'Mrs. Understood',
+        username: 'what',
+        email: 'what@robots.de'
+    }
+]
