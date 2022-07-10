2cb166fc09c915b8e18041d3187a0386cdc309dc
thejohnq <floschweig@gmail.com>
2022-07-10 17:46:45 +0200

    Updates

---
+++ precache-manifest.faa1b282fc083e0a86aa786caaf9df4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b233e7ce53ef825e2c0",
+    "url": "/robofriends/static/css/main.aa9dda44.chunk.css"
+  },
+  {
+    "revision": "9b233e7ce53ef825e2c0",
+    "url": "/robofriends/static/js/main.9b233e7c.chunk.js"
+  },
+  {
+    "revision": "16c2c63494c8fd9548be",
+    "url": "/robofriends/static/css/1.5236be9b.chunk.css"
+  },
+  {
+    "revision": "16c2c63494c8fd9548be",
+    "url": "/robofriends/static/js/1.16c2c634.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6612e6690c4881100d42fdf19e3dee13",
+    "url": "/robofriends/index.html"
+  }
+];
